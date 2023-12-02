56b399cf1a07603edde7a617e25203debdea8414
Mateusz Warzecha <warzecha.mati@gmail.com>
2023-12-02 21:30:41 +0100

    headless working, google auth still blocking

--- web_scrape.js
+++ web_scrape.js
@@ -1,5 +1,9 @@
-const { fstat } = require('fs');
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
+const chromium = require('chrome-aws-lambda');
+const pluginStealth = require('puppeteer-extra-plugin-stealth')();
+
+puppeteer.use(pluginStealth);
+pluginStealth.setMaxListeners = () => {};
 
 function delay(time) {
     return new Promise(function(resolve) { 
@@ -10,9 +14,14 @@ function delay(time) {
 (async ()=>{
     const browser = await puppeteer.launch({
         slowMo: 100,
-        headless: false
+        // headless: false,
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        ignoreHTTPSErrors: true,
     });
     const page = await browser.newPage();
+    await page.setBypassCSP(true);
 
     await page.goto("https://ai.invideo.io/login");
     const newPagePromise = new Promise(x => browser.once('targetcreated', target => x(target.page())));
@@ -20,9 +29,11 @@ function delay(time) {
     const googleAuth = await newPagePromise;           // open new tab /window, 
 
     await delay(4000);
-    await googleAuth.waitForSelector('.zHQkBf', { timeout: 5_000 });
-    await googleAuth.type('.zHQkBf','infinity.ai.master@gmail.com', {delay: 70})
-    await googleAuth.click('.VfPpkd-LgbsSe-OWXEXe-k8QpJ');
+    // await googleAuth.type('.zHQkBf','infinity.ai.master@gmail.com', {delay: 70})
+    await googleAuth.type('#identifierId','infinity.ai.master@gmail.com', {delay: 70})
+
+    // await googleAuth.click('.VfPpkd-LgbsSe-OWXEXe-k8QpJ');
+    await googleAuth.click('#identifierNext');
     await delay(4000);
     // await googleAuth.type('.zHQkBf','PASSWORD', {delay: 70}) // PASSWORD VISABLE
     
