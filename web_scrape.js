64165b8050bb2013cdb7626fe1bc38bba4ad4b36
Mateusz Warzecha PC <warzecha.mati@gmail.com>
2023-12-12 00:04:21 +0100

    video making process working

--- web_scrape.js
+++ web_scrape.js
@@ -45,26 +45,60 @@ function delay(time) {
         // executablePath: await chromium.executablePath,
         // ignoreHTTPSErrors: true,
     });
+
+    // Opening browser
     const page = await browser.newPage();
     await page.setBypassCSP(true);
 
+    // Opening page
     await page.goto("https://app.pictory.ai/login");
-    await delay(3000);
+    await delay(5000);
 
+    // Logging in
     await page.type('#mui-1', pictoryLogin);
     await page.type('#outlined-adornment-password', pictoryPassword);
     await page.click('.css-1du8a1u', );
     await delay(10000);
     
+    // Declaring video making method
     await page.click('.script-to-video-button');
+    await delay(5000);
+
+    // Data insert
     await page.type('.script-video-name input', dummy_data.title );
-    
     await page.$$eval('.ck-editor__editable p', (links, value) => links.forEach(el => el.innerHTML = value), 'Unlock Your Potential. Embrace the Challenges. Persist Through Adversity. Dream Big, Work Hard. Failure is a Stepping Stone. Celebrate Your Victories. Stay Focused, Stay Committed. Inspire Others with Your Journey. Success is a Journey, Not a Destination');
     await delay(10000);
 
-    // await page.click('.css-8aqpyn');
+    // Proceed
+    await page.click('.css-8aqpyn .css-1he72jf');
+    await delay(5000);
 
+    // Choosing Theme
+    await page.click('#template_1d550871-3707-4503-9e4b-367dd849bfc7');
+    await delay(5000);
+
+    // Choosing aspect ratio
+    await page.click('.css-1x97c6v .css-1lekzkb .css-79elbk');
+    await delay(25000);
+
+    // Expanding voiceover menu
+    await page.click('#voiceover-menu-button');
+    await delay(5000);
     
+    // Choosing aspect ratio
+    await page.click('#voiceover-menu li');
+    await delay(5000);
+    
+    // Close popup advertisement
+    await page.click('.voice-over-banner div svg');
+    
+    // Choosing voice model
+    await page.hover('#voiceTrack1009');
+    await page.screenshot({ path: 'hover.png' })
+    await page.click('#voiceTrack1009 .css-1g747ue .apply-box span');
+    await delay(10000);
+
+
     
     await page.screenshot({ path: 'example.png' });
 
