9d87831000d2b51ee7b476af29d760155a073758
Bart <barteqczech@gmail.com>
2024-01-03 16:53:40 +0100

    userModel & connectDB done

---
+++ MongoDB/connectDB.js
@@ -0,0 +1,18 @@
+// MongoDB/config/db.js
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
