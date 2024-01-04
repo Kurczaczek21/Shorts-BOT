9f2d6163250f1625d892a190157e7c94770bee31
Bart <barteqczech@gmail.com>
2024-01-04 12:05:04 +0100

    Server correctly decrypts the token, error while sending the page

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ require("dotenv").config();
 const cors = require("cors");
 const Dotenv = require("dotenv-webpack");
 const { chatWithOpenAI } = require("./chatGPT_API");
+const User = require("./backend/MongoDB/userModel");
+const jwt = require('jsonwebtoken');
+
 
 const connectDB = require("./backend/MongoDB/connectDB");
-const loggedInRouter = require('./backend/routes/loggedInRoute');
-const authRoutes = require('./backend/MongoDB/auth');
-const registerRoutes = require('./backend/MongoDB/register');
+const loggedInRouter = require("./backend/routes/loggedInRoute");
+const authRoutes = require("./backend/MongoDB/auth");
+const registerRoutes = require("./backend/MongoDB/register");
 
 module.exports = {
   // Konfiguracja webpack
@@ -24,9 +27,12 @@ app.use(cors());
 app.use(express.json());
 
 // Login & Register
-app.use('/login', authRoutes);
-app.use('/register', registerRoutes);
-app.use('/logged-in', loggedInRouter);
+app.use("/login", authRoutes);
+app.use("/register", registerRoutes);
+app.get("/logged-in", authenticateToken, (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.sendFile(__dirname + "/UI/after-login.html");
+});
 
 app.use(express.static(__dirname + "/UI"));
 
@@ -67,3 +73,33 @@ app.listen(port, () => {
 app.get("/home", (req, res) => {
   res.sendFile(__dirname + "/UI/home-page.html");
 });
+
+// tests
+app.get("/tests", authenticateToken, (req, res) => {
+  res.json("Nice");
+});
+
+app.post("/test-login", async (req, res) => {
+  const username = req.body.username;
+  const user = await User.findOne({ username });
+  const id = user._id
+  console.log(id);
+  const accessToken = jwt.sign({id}, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  })
+  res.json({ accessToken: accessToken });
+});
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization']
+  console.log(authHeader)
+  const token = authHeader // && authHeader.split(' ')[1]
+  console.log("TOken: " + token)
+  if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(418)
+    req.user = user
+    next()
+  })
+}
