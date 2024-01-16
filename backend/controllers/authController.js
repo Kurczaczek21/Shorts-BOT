0953baf2873a8866886973df76398067d35cab42
Mateusz Warzecha PC <warzecha.mati@gmail.com>
2024-01-16 01:21:30 +0100

    signin api

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require("../MongoDB/userModel");
 const bcrypt = require("bcrypt");
 const errorHandler = require("../utils/error");
+const jwt = require("jsonwebtoken");
 
 const signup = async (req, res, next) => {
   const { username, password } = req.body;
@@ -10,9 +11,28 @@ const signup = async (req, res, next) => {
     await newUser.save();
     res.status(201).json({ message: "User created." });
   } catch (error) {
-    // next(error);
-    next(errorHandler(300, "cos"));
+    next(error);
+    // next(errorHandler(300, "cos"));
   }
 };
 
-module.exports = signup;
+const signin = async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const validUser = await User.findOne({ username });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Incorrect password"));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET); // secret - random string for sec
+    const { password: hashedPassword, ...restOfUser } = validUser._doc; // removing password from response
+    res
+      .cookie("accessToken", token, { httpOnly: true })
+      .status(200)
+      .json(restOfUser); // httpOnly - true -> prevent 3rd party apps to modify cookie ez
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { signup, signin };
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const signup = require("../controllers/authController");
+const {signup, signin} = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 module.exports = router;
